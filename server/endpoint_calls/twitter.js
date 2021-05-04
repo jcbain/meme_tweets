3533e1ff21a28549ab830932279b3d2fedfd7b71
jcbain <jamescbain@gmail.com>
2021-05-04 15:10:02 -0600

    start cleaning up routes

--- server/endpoint_calls/twitter.js
+++ server/endpoint_calls/twitter.js
@@ -12,6 +12,19 @@ const defaultParams = {
     ]
 }
 
+const getConversation = async (conversationId, isHistorical, nextToken, params=defaultParams) => {
+    const headers = createHeaders(isHistorical);
+    const baseUrl = chooseStream(isHistorical);
+    const query = `query=conversation_id:${conversationId}`;
+    const nextString = nextToken ? `next_token=${nextToken}` : '';
+    const { startString, endString, fieldsString } = formatParams(params);
+    const endpointString = [query, startString, endString, fieldsString, nextString].join('&');
+    const fullEndpoint = `${baseUrl}?${endpointString}`;
+    const response = await axios.get(fullEndpoint, headers);
+
+    return response;
+}
+
 
 const getConversationByGeography = async (conversationId, lon, lat, nextToken, params=defaultParams) => {
     const headers = createHeaders(true);
@@ -27,9 +40,14 @@ const getConversationByGeography = async (conversationId, lon, lat, nextToken, p
     return response;
 }
 
+module.exports = {
+    getConversation,
+    getConversationByGeography
+}
 
-getConversationByGeography('1384253384245465100', -74.0060, 40.7128)
-    .then(resp => console.log(resp.data))
-    .catch(err => console.log(err))
+
+// getConversationByGeography('1384253384245465100', -74.0060, 40.7128)
+//     .then(resp => console.log(resp.data))
+//     .catch(err => console.log(err))
 
     // https://api.twitter.com/2/tweets/search/all?start_time=2015-01-01T00:00:00Z&tweet.fields=id,text,author_id,context_annotations,geo,conversation_id,withheld,possibly_sensitive,referenced_tweets,public_metrics,created_at&query=(point_radius:[-74.0060 40.7128 25mi] conversation_id:1384253384245465100)&end_time=2021-04-21T00:00:00Z
--- server/index.js
+++ server/index.js
@@ -3,21 +3,37 @@ const cors = require('cors');
 const app = express();
 const port = 8000;
 
+const cities = require('./data/cities.json')
+
+const { getConversation, getConversationByGeography } = require('./endpoint_calls/twitter');
+
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/tweets/conversations/:id', (req, res) => {
+app.get('/tweets/conversations/:id', async (req, res) => {
     const { id } = req.params;
-    res.send('hello')
-    console.log(id)
+    const result = await getConversation(id, true)
+    console.log(result.data)
 
 })
 
-app.get('/tweets/conversations/:id/geo/:lon/:lat', (req, res) => {
-    const { id, lon, lat } = req.params;
-    console.log(id, lon, lat)
 
+app.get('/tweets/conversations/:id/geo', async (req, res) => {
+    const { id } = req.params;
+    console.log(cities)
+
+    // const result = await getConversationByGeography(id, lon, lat)
+
+    // console.log(result.data)
+})
+
+app.get('/tweets/conversations/:id/geo/:lon/:lat', async (req, res) => {
+    const { id, lon, lat } = req.params;
+
+    const result = await getConversationByGeography(id, lon, lat)
+
+    console.log(result.data)
 })
 
 app.listen(port, () => {
